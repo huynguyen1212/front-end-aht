e719a6ae711afa4bb84d3c607196c86386c78073
Nguyen Quang Huy <nguyenquanghuy@Nguyens-MacBook-Pro.local>
2022-07-07 10:01:52 +0700

    init

---
+++ contest4/src/containers/Home/data.ts
@@ -0,0 +1,53 @@
+import imgitem from 'assets/images/img_item.png';
+
+export const dataProduct = [
+  {
+    image: imgitem,
+    name: 'Body Champ Cardio',
+    price: '$342.00',
+  },
+  {
+    image: imgitem,
+    name: 'Body Solid GCEC340',
+    price: '$214.00',
+  },
+  {
+    image: imgitem,
+    name: 'Body Solid GCEC340',
+    price: '$214.00',
+  },
+  {
+    image: imgitem,
+    name: 'Body Solid GCEC340',
+    price: '$214.00',
+  },
+  {
+    image: imgitem,
+    name: 'Body Solid GCEC340',
+    price: '$214.00',
+  },
+  {
+    image: imgitem,
+    name: 'Body Solid GCEC340',
+    price: '$214.00',
+  },
+  {
+    image: imgitem,
+    name: 'Body Solid GCEC340',
+    price: '$214.00',
+  },
+  {
+    image: imgitem,
+    name: 'Body Solid GCEC340',
+    price: '$214.00',
+  },
+];
+
+export const dataContact = ['My account', 'check out', 'register', 'Login'];
+export const dataInfor = [
+  'About us',
+  'Contact us',
+  'Terms of user',
+  'Privacy Policy',
+];
+export const dataQuick = ['New user', 'Help center', 'Report spam', 'FAQs'];
